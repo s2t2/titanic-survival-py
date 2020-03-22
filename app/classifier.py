665cc4710c6f211c92e8b8996ada109d5dc7547c
Mike Rossetti <s2t2@users.noreply.github.com>
2020-03-22 19:58:03 -0400

    Refactor and reorganize

--- app/classifier.py
+++ app/classifier.py
@@ -50,19 +50,18 @@ def train_model():
 
     print("-----------------")
     print("LOADING DATA...")
-    print("-----------------")
+
     importer = Importer()
     xtrain, ytrain = filter_features(importer.xtrain), importer.ytrain
     xval, yval = filter_features(importer.xval), importer.yval
     xtest, xtest_passenger_ids = filter_features(importer.xtest), importer.xtest_passenger_ids
-    print(importer)
-    print("TRAINING SET:", xtrain.shape, ytrain.shape)
-    print("VALIDATION SET:", xval.shape, yval.shape)
-    print("TEST SET:", xtest.shape, len(xtest_passenger_ids))
+    print("TRAINING:", xtrain.shape, ytrain.shape)
+    print("VALIDATION:", xval.shape, yval.shape)
+    print("TEST:", xtest.shape, len(xtest_passenger_ids))
 
     print("-----------------")
     print("CONFIGURING SEARCH PARAMETERS...")
-    print("-----------------")
+
     pipeline = Pipeline(steps=[
         ("one_hot", OneHotEncoder(use_cat_names=True, cols=["gender"])),
         ("ordinal", OrdinalEncoder(cols=["ticket_class"], mapping=[{"col": "ticket_class", "mapping": {"UPPER":3, "MIDDLE":2, "LOWER":1}}])),
@@ -70,93 +69,74 @@ def train_model():
         ("scaler", StandardScaler()),
         ("classifier", DecisionTreeClassifier(random_state=89))
     ])
-    print(pipeline)
+    print(type(pipeline))
+
     search = GridSearchCV(estimator=pipeline, cv=5, verbose=10, return_train_score=True, n_jobs=-1, # -1 means using all processors
         scoring="accuracy", #"f1",
         param_grid={
-            #"imputer__strategy": ["mean", "median"],
+            "imputer__strategy": ["mean", "median"],
             "classifier__min_samples_leaf": (0.02, 0.03, 0.06),
             "classifier__max_depth": (1, 2, 3, 4, 5),
         }
     )
-    print(search)
+    print(type(search))
 
     print("-----------------")
     print("TRAINING...")
-    print("-----------------")
+
     search.fit(xtrain, ytrain)
-    training_score = search.score(xtrain, ytrain)
-
-    print("-----------------")
-    print("SAVING MODEL:")
-    print(os.path.abspath(LATEST_MODEL_FILEPATH))
-    with open(LATEST_MODEL_FILEPATH, "wb") as f:
-        pickle.dump(search, f)
-
-    print("-----------------")
-    print("GENERATING PREDICTIONS (VALIDATION):")
-    yval_predictions = search.predict(xval)
-    report = classification_report(yval, yval_predictions)
-    print(report)
-
-    print("-----------------")
-    print("GENERATING PREDICTIONS (TEST):")
-    print(os.path.abspath(PREDICTIONS_FILEPATH))
-    ytest_predictions = search.predict(xtest)
-    ytest_predictions_df = DataFrame({"PassengerId": xtest_passenger_ids, "Survived": ytest_predictions})
-    ytest_predictions_df.to_csv(PREDICTIONS_FILEPATH, index=False)
-
-    print("-----------------")
-    print("SCORING...")
-    print("-----------------")
 
     best_estimator = search.best_estimator_ #> Pipeline
     one_hot = best_estimator.named_steps["one_hot"]
     ordinal = best_estimator.named_steps["ordinal"]
-    #imputer = best_estimator.named_steps["imputer"]
-    #scaler = best_estimator.named_steps["scaler"]
     model = search.best_estimator_.named_steps["classifier"]
 
-    class_names = [str(class_name) for class_name in search.classes_] #> ["0", "1"]
-    feature_importances = model.feature_importances_
-    # reverse-engineer feature names, as they've been modified during encoding:
-    xval_encoded = ordinal.transform(one_hot.transform(xval)) # apply one-hot first, then ordinal
-    feature_names = xval_encoded.columns # operating on a dataframe
-    encoded_features = Series(feature_importances, feature_names).sort_values()
-    encoded_features.name = "encoded_feature"
-
-    results = {
-        "class_names": class_names,
-        "step_names": [step_name for step_name, obj in best_estimator.steps],
-        "best_score": search.best_score_,
-        "best_params": search.best_params_,
-        "training_score": training_score,
-        "validation_score": search.score(xval, yval),
-        "feature_importances": encoded_features.to_dict(),
-    }
-    pprint(results)
-    with open(SEARCH_RESULTS_FILEPATH, "w") as f:
-        f.write(json.dumps(results))
+    encoded_feature_names = ordinal.transform(one_hot.transform(xval)).columns
+    encoded_feature_importances = Series(model.feature_importances_, encoded_feature_names).sort_values(ascending=False)
+    encoded_feature_importances.name = "encoded_feature"
 
     print("-----------------")
-    print("GRAPHING FEATURE IMPORTANCES:")
+    print("BEST SEARCH PARAMS:", search.best_params_)
+    print("BEST SEARCH SCORE:", search.best_score_)
+
+    print("-----------------")
+    training_score = search.score(xtrain, ytrain)
+    print("TRAINING SCORE:", training_score)
+
+    print("-----------------")
+    validation_score = search.score(xval, yval)
+    print("VALIDATION SCORE:", validation_score)
+
+    print("-----------------")
+    print("VALIDATION REPORT:")
+    yval_predicted = search.predict(xval)
+    validation_report = classification_report(yval, yval_predicted)
+    print(validation_report)
+
+    print("-----------------")
+    print("FEATURE IMPORTANCES:")
+    print(encoded_feature_importances)
+
+    print("-----------------")
+    print("FEATURE IMPORTANCES VIZ:")
     print(os.path.abspath(FEATURES_GRAPH_FILEPATH))
     fig = px.bar(orientation="h", # horizontal flips x and y params below, but not labels
-        x=encoded_features.values,
-        y=encoded_features.keys(),
+        x=encoded_feature_importances.values,
+        y=encoded_feature_importances.keys(),
         labels={"x": "Relative Importance", "y": "Feature Name"}
     )
     #fig.show()
     fig.write_image(FEATURES_GRAPH_FILEPATH) # requires "orca" dependency. see: https://plot.ly/python/static-image-export/
 
     print("-----------------")
-    print("GRAPHING DECISION TREE:")
+    print("DECISION TREE VIZ:")
     print(os.path.abspath(TREE_GRAPH_FILEPATH))
+    class_names = [{0:"Not Survive", 1:"Survive"}[class_name] for class_name in search.classes_] # [0, 1]
     dot_data = export_graphviz(model,
         out_file=None,
         max_depth=3,
-        feature_names=feature_names,
-        class_names=class_names,
+        feature_names=encoded_feature_names,
+        class_names=class_names, # expects class names to be strings
         impurity=False,
         filled=True,
         proportion=True,
@@ -166,6 +146,34 @@ def train_model():
     png_bytes = graph.pipe(format="png")
     with open(TREE_GRAPH_FILEPATH, "wb") as f:
         f.write(png_bytes)
+
+    print("-----------------")
+    print("SAVING RESULTS:")
+    print(os.path.abspath(SEARCH_RESULTS_FILEPATH))
+    results = {
+        "pipeline_steps": [step_name for step_name, obj in best_estimator.steps],
+        "best_score": search.best_score_,
+        "best_params": search.best_params_,
+        "training_score": training_score,
+        "validation_score": validation_score,
+        "encoded_feature_importances": encoded_feature_importances.to_dict(),
+    }
+    with open(SEARCH_RESULTS_FILEPATH, "w") as f:
+        f.write(json.dumps(results))
+
+    print("-----------------")
+    print("SAVING MODEL:")
+    print(os.path.abspath(LATEST_MODEL_FILEPATH))
+    with open(LATEST_MODEL_FILEPATH, "wb") as f:
+        pickle.dump(search, f)
+
+    print("-----------------")
+    print("GENERATING SUBMISSION FILE:")
+    print(os.path.abspath(PREDICTIONS_FILEPATH))
+
+    ytest_predictions = search.predict(xtest)
+    ytest_predictions_df = DataFrame({"PassengerId": xtest_passenger_ids, "Survived": ytest_predictions})
+    ytest_predictions_df.to_csv(PREDICTIONS_FILEPATH, index=False)
 
     return search
 
